63c361f42e50a4262357795d815fe4a516de02e7
Yizhou Li <yizhoul316@hotmail.com>
2024-01-01 22:02:14 -0500

    initial commit

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  title: '4chuan McChicken Blog',
+  author: 'Yizhou Li',
+  headerTitle: '4chuan McChicken Blog',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
